8bde399be760816c5ea374b36dc11512e73529cf
LAKSHMAN S <lakshmans218@gmail.com>
2024-08-06 20:19:02 +0530

    solution for 2475

---
+++ 2475.js
@@ -0,0 +1,17 @@
+var unequalTriplets = function(nums) {
+  let count=0
+  for(let i=0;i<nums.length;i++){
+      let e1=nums[i]
+      for(let j=i+1;j<nums.length;j++){
+          let e2= nums[j]
+          if(e1==e2) continue
+          for(let k=j+1;k<nums.length;k++){
+              let e3=nums[k]
+              if(e1!==e3 && e2!==e3){
+                  count++
+              }
+          }
+      }
+  }
+  return count
+};
