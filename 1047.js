6032e8dd9b0cc0371f724797753dd493592b5429
LAKSHMAN S <lakshmans218@gmail.com>
2025-07-13 22:50:17 +0530

    solution for 1047

---
+++ 1047.js
@@ -0,0 +1,16 @@
+var removeDuplicates = function(s) {
+    let stack = [];
+
+    for (let char of s) {
+        if (stack.length && stack[stack.length - 1] === char) {
+            stack.pop(); 
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.join('');
+};
+
+const s = "abbaca"
+console.log(removeDuplicates(s));
